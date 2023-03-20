881514c8223fc6260a9f75fe53bdb0f9e9796868
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-20 17:29:55 -0300

    componente home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import {MyRoutes} from "./routes/routes"
+
 function App() {
   const [count, setCount] = useState(0)
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
 
-
-export function Home() {
+export function Home(props) {
     return (
-      <h1> Home</h1>
-    );
+        <div>
+        <div>
+          <div>
+            <h1>
+              <Link to="/login">Login</Link>
+            </h1>
+            <br />
+            <h1>
+              <Link to="/signup">Registrar</Link>
+            </h1>
+          </div>
+        </div>
+        <h2>{props.name?`Bienvenido - ${props.name}`:"Inicie sesión"}</h2>
+        <button>Salir</button>
+       
+      </div>
+      )
   }
