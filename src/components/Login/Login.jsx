177c5639588cb5e337347b58f014485154736f78
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-20 17:39:52 -0300

    componente login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,24 @@
+import styles from "./Login.module.css";
+import { Link } from "react-router-dom";
+
 export function Login (){
     return(
-        <h1>Login</h1>
+        <div className={styles.container}>
+          <div className={styles.innerBox}>
+          <h1 className={styles.heading}>Login</h1>
+         
+          <div className={styles.footer}>
+          <b className={styles.error}>Error</b>
+          <button>Login btn</button>
+          <p>
+            Crear cuenta
+            <span>
+              <Link to="/signup"> Ir</Link>
+            </span>
+          </p>
+        </div>
+        </div>
+        </div>
+        
     )
 }
